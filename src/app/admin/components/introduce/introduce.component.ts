bbe430d7ad4e9a158fedb7ab8734a45d4aee44d0
tintrunghuynh <tintrunghuynh.alviss@gmail.com>
2019-11-07 20:34:15 +0700

    auto-deploy

--- src/app/admin/components/introduce/introduce.component.ts
+++ src/app/admin/components/introduce/introduce.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, HostListener } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 @Component({
     selector: "app-introduce",
@@ -14,7 +14,25 @@ export class IntroduceComponent implements OnInit {
     constructor(private route: ActivatedRoute) { }
 
     ngOnInit() {
-
+        this.setHeightOfRow2();
+        this.loadingBackgroundRow1();
+    }
+    @HostListener("window:resize", ["$event"])
+    onResize(event?) {
+        this.setHeightOfRow2();
     }
 
+    setHeightOfRow2() {
+        const heightRow1 = $("#row1").css("height");
+        $("#row2").css("height", `calc(${heightRow1} / 17 * 50)`);
+        // console.log(`row1.height = ${$("#row1").css("height")}`);
+        // console.log(`row2.height = ${$("#row2").css("height")}`);
+    }
+
+    loadingBackgroundRow1() {
+        const bgArray = ["keyboard.jpg", "7.jpg"];
+        const bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+        console.log(bg);
+        $("#row1").css("background-image", `url("/assets/Imgs/${bg}")`);
+    }
 }
